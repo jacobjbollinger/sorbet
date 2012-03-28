8de539e5fb60f84f0e911e8b416f4a03d23bb466
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2012-03-28 22:53:51 +0200

    use UTC time zone for dates stored in the database
    feedparser already converts dates into UTC so we just have to make
    sure that Django is aware of that fact. Closes #16

--- sorbet/core/tasks.py
+++ sorbet/core/tasks.py
@@ -1,4 +1,6 @@
 from django.core.exceptions import ObjectDoesNotExist
+from django.utils.timezone import now as tz_utcnow
+from django.utils.timezone import utc 
 
 from celery.task import task
 from feedparser import parse
@@ -14,7 +16,6 @@ def fetch_all_feeds():
     for feed in feeds:
         fetch_feed_items(feed)
 
-
 @task
 def fetch_feed_items(feed):
     parsed_feed = parse(feed.url)
@@ -24,15 +25,15 @@ def fetch_feed_items(feed):
         except ObjectDoesNotExist:
             new_item = Item(
                 feed = feed,
-                pubdate = datetime(*(item.published_parsed[0:6])),
+                pubdate = datetime(*(item.published_parsed[0:6]), tzinfo=utc),
                 title = item.title,
                 link = item.link,
                 description = item.summary,
                 guid = item.id,
             )
-            new_item.save()
+            #new_item.save()
             # TODO: Run task to send item to everyone subscribing to feed.
 
-    feed.last_updated = datetime(*(parsed_feed.updated_parsed[0:6]))
-    feed.last_checked = datetime.now()
+    feed.last_updated = datetime(*(parsed_feed.updated_parsed[0:6]), tzinfo=utc)
+    feed.last_checked = tz_utcnow()
     feed.save()
