9d4262504a6198b35821fa62d113e180d94394c9
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2012-03-28 22:17:49 +0200

    use a custom Auth form to get a proper label

--- sorbet/core/urls.py
+++ sorbet/core/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import patterns, url
 
+from sorbet.core.forms import EmailAuthenticationForm
+
 
 urlpatterns = patterns('',
     url(r'^$', 'sorbet.core.views.home', name='home'),
     url(r'^accounts/register/$', 'sorbet.core.views.register', name='register'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'core/login.html'}, name='login'),
+        {'template_name': 'core/login.html',
+         'authentication_form': EmailAuthenticationForm}, name='login'),
     url(r'^accounts/logout/$', 'sorbet.core.views.logout_user', name='logout'),
     url(r'^featured/$', 'sorbet.core.views.featured', name='featured'),
     url(r'^feeds/$', 'sorbet.core.views.feeds', name='feeds'),
