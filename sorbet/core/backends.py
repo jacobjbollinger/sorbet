60e6877602c105b0c9325169414f2f5d248d0997
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2012-03-28 22:12:00 +0200

    Revert "check the correct field for username"
    This reverts commit 9bbeafb0b5567f5c466a911f3cf0760170b70cde.

--- sorbet/core/backends.py
+++ sorbet/core/backends.py
@@ -12,7 +12,7 @@ class EmailAuthBackend(object):
 
     def authenticate(self, username=None, password=None):
         try:
-            user = User.objects.get(username=username)
+            user = User.objects.get(email=username)
             if user.check_password(password):
                 return user
         except User.DoesNotExist:
