14cbe770b45fa87e52d7b73054a056a3e9d729a6
Krzysztof Klimonda <kklimonda@syntaxhighlighted.com>
2012-04-10 20:38:14 +0200

    and commit the model part of the previous commit

--- sorbet/core/models.py
+++ sorbet/core/models.py
@@ -6,4 +6,4 @@ class Invitation(models.Model):
     def _create_uuid():
         return str(uuid4())
 
-    key = models.CharField(max_length=32, default=_create_uuid, primary_key=True)
+    key = models.CharField(max_length=36, default=_create_uuid, primary_key=True)
